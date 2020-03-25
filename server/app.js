c3e4221a43d1a8e0571984fd851d90432e81a63d
big13333 <big49516@gmail.com>
2020-03-25 22:00:58 +0700

    Project

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const API_PORT = 3002;
+const API_PORT = 3000;
 
 
 //mongoDB CONNECT
