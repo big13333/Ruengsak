43ff3569eeaa0edcc8af437222885199ab14c6c6
big13333 <big49516@gmail.com>
2020-03-25 15:31:22 +0700

    Project

--- src/App.js
+++ src/App.js
@@ -31,37 +31,29 @@ class App extends Component {
             <div className="logo" />
             <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
               <Menu.Item key="1">
-                <Icon type="pie-chart" />
-                <span>ITEM 1</span>
+                <Icon type="appstore" />
+                <span>Root of Equation</span>
                 <Link to='/TAP1' />
               </Menu.Item>
               <Menu.Item key="2">
-                <Icon type="desktop" />
-                <span>ITEM 2</span>
+                <Icon type="appstore" />
+                <span>Linear Algebra</span>
                 <Link to='/TAP2' />
               </Menu.Item>
               <Menu.Item key="3">
-                <Icon type="smile" theme="outlined" />
-                <span>ITEM 3</span>
+                <Icon type="appstore" theme="outlined" />
+                <span>Simson</span>
                 <Link to='/TAP3' />
               </Menu.Item>
               <Menu.Item key="4">
-                <Icon type="smile" theme="outlined" />
-                <span>ITEM 4</span>
+                <Icon type="appstore" theme="outlined" />
+                <span>Integration</span>
                 <Link to='/TAP4' />
               </Menu.Item>
               <Menu.Item key="5">
-                <Icon type="smile" theme="outlined" />
-                <span>ITEM 5</span>
+                <Icon type="appstore" theme="outlined" />
+                <span>Differentiation</span>
                 <Link to='/TAP5' />
-              </Menu.Item>
-              <Menu.Item key="6">
-                <Icon type="smile" theme="outlined" />
-                <span>ITEM 6</span>
-              </Menu.Item>
-              <Menu.Item key="7">
-                <Icon type="smile" theme="outlined" />
-                <span>ITEM 7</span>
               </Menu.Item>
             </Menu>
           </Sider>
